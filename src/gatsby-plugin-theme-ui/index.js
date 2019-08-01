6f10b53b4d0cf84aff6bfdb77efdcce46294d12a
Allan Pooley <allanpooley@outlook.com>
2019-08-01 17:15:33 +1000

    Change gatsby-theme-ui to gatsby-plugin-theme-ui

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,17 +1,9 @@
-import merge from 'lodash.merge';
-import { theme } from '@littleplusbig/gatsby-theme-legals-prismic';
+import baseTheme from '@littleplusbig/gatsby-theme-legals-prismic';
 
-export default merge({}, theme, {
+export default {
+  ...baseTheme,
   colors: {
-    text: '#000000',
-    background: '#000000',
-    primary: '#000000',
-    primaryLighter: '#000000',
-    primaryEvenLighter: '#000000',
-    white: '#000000',
-    offWhite: '#000000',
-    black: '#000000',
-    offBlack: '#000000',
-    grey: '#000000',
+    ...baseTheme.colors,
+    primary: '#000',
   },
-});
+};
