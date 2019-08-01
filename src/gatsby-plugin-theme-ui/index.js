877d19a4dd0a8d93b41577faad3c4a9cb9f60b39
Allan Pooley <allanpooley@outlook.com>
2019-08-01 17:45:18 +1000

    It's working!!

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -2,8 +2,22 @@ import baseTheme from '@littleplusbig/gatsby-theme-legals-prismic';
 
 export default {
   ...baseTheme,
+  fonts: {
+    ...baseTheme.fonts,
+    body: '-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, sans-serif',
+    heading: '-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, sans-serif',
+  },
   colors: {
     ...baseTheme.colors,
-    primary: '#000',
+    text: '#333333',
+    background: '#FFFFFF',
+    primary: '#22a6b3',
+    primaryLighter: '#7ed6df',
+    primaryEvenLighter: '#c7ecee',
+    white: '#FFFFFF',
+    offWhite: '#FCFAFF',
+    black: '#000000',
+    offBlack: '#333333',
+    grey: '#F3F3F3',
   },
 };
