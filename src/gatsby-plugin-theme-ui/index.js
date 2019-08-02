a2a0fc5e07a42a91b51d3cc9d0197cf3a5935087
Allan Pooley <allanpooley@outlook.com>
2019-08-02 10:56:43 +1000

    Applied example theme change to legal docs

---
+++ src/gatsby-plugin-theme-ui/index.js
@@ -0,0 +1,11 @@
+import baseTheme from '@littleplusbig/gatsby-theme-legals-prismic/src/gatsby-plugin-theme-ui';
+
+export default {
+  ...baseTheme,
+  colors: {
+    ...baseTheme.colors,
+    primary: '#22a6b3',
+    primaryLighter: '#7ed6df',
+    primaryEvenLighter: '#c7ecee',
+  },
+};
